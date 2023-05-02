2a4d891159931e51a408a337a69dba879f68f6d1
Ronit Zinzuvadiya <96730598+RonitZinzuvadiya1040@users.noreply.github.com>
2023-05-03 01:10:32 +0530

    Update Task9.php

--- Rankway/Array Task/Task9.php
+++ Rankway/Array Task/Task9.php
@@ -1,3 +1,13 @@
+<!-- 
+    A: create array for lastname values 
+
+    $arr = array( 
+                array('id' => 5698, 'first_name' => 'Peter', 'last_name' => 'Griffin',),
+                array('id' => 4767, 'first_name' => 'Ben', 'last_name' => 'Smith',),
+                array('id' => 3809, 'first_name' => 'Joe', 'last_name' => 'Doe',)
+            ); 
+-->
+
 <?php
 
 $arr = array(
