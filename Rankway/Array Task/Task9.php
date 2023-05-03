183e209a6654a7c6789583fea7b56229c19a66be
Ronit Zinzuvadiya <96730598+RonitZinzuvadiya1040@users.noreply.github.com>
2023-05-03 10:22:06 +0530

    Update Task9.php

--- Rankway/Array Task/Task9.php
+++ Rankway/Array Task/Task9.php
@@ -10,30 +10,30 @@
 
 <?php
 
-$arr = array(
-    array(
-    'id' => 5698,
-    'first_name' => 'Peter',
-    'last_name' => 'Griffin',
-    ),
-    array(
-    'id' => 4767,
-    'first_name' => 'Ben',
-    'last_name' => 'Smith',
-    ),
-    array(
-    'id' => 3809,
-    'first_name' => 'Joe',
-    'last_name' => 'Doe',
-    )
-    );
+    $arr = array(
+        array(
+        'id' => 5698,
+        'first_name' => 'Peter',
+        'last_name' => 'Griffin',
+        ),
+        array(
+        'id' => 4767,
+        'first_name' => 'Ben',
+        'last_name' => 'Smith',
+        ),
+        array(
+        'id' => 3809,
+        'first_name' => 'Joe',
+        'last_name' => 'Doe',
+        )
+        );
 
-$last_names = array(); // initialize an empty array
+    $last_names = array(); // initialize an empty array
 
-foreach ($arr as $value) {
-    $last_names[] = $value['last_name']; // add last name to the array
-}
-echo "<pre>";
-print_r($last_names); // print the array of last names
+    foreach ($arr as $value) {
+        $last_names[] = $value['last_name']; // add last name to the array
+    }
+    echo "<pre>";
+    print_r($last_names); // print the array of last names
 
 ?>
