f4a7f2402853b12215ea9b2230dc0705ea31717f
Ronit Zinzuvadiya <96730598+RonitZinzuvadiya1040@users.noreply.github.com>
2023-05-03 13:20:37 +0530

    Update Task8.php

--- Rankway/Array Task/Task8.php
+++ Rankway/Array Task/Task8.php
@@ -3,6 +3,8 @@
     A: sort arrays into single array 
 -->
 
+<!-- Method:1 Using in-built function -->
+
 <?php
 
     $arr = ['mmm', ["35", "37", ['23'], ['90', ['569', ['96']]]], ["43", "30"], ["24", "47"], ["27"]];
@@ -19,4 +21,27 @@
     echo "<pre>";
     print_r($mergedArr);
     
+?>
+
+<!-- Method:2 Using custom function -->
+
+<?php
+
+    $arr = ['mmm', ["35", "37", ['23'], ['90', ['569', ['96']]]], ["43", "30"], ["24", "47"], ["27"]];
+
+    $mergedArr = [];
+
+    function singleArray($array, &$result) {
+        foreach($array as $value) {
+            if(is_array($value)) {
+                singleArray($value, $result);
+            } else {
+                $result[] = $value;
+            }
+        }
+    }
+    singleArray($arr, $mergedArr);
+    echo "<pre>";
+    print_r($mergedArr);
+
 ?>
