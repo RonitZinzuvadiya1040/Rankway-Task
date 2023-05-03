8353f44dbf242510d9c6f11252e2b8fb8307d522
Ronit Zinzuvadiya <96730598+RonitZinzuvadiya1040@users.noreply.github.com>
2023-05-03 15:11:49 +0530

    Update Task6.php

--- Rankway/Array Task/Task6.php
+++ Rankway/Array Task/Task6.php
@@ -3,6 +3,8 @@
     A:Create array with 3(n) values like [ ["35","37","43"], ["30","24"]] using function 
 -->
 
+<!-- Method-1: using function -->
+
 <?php
     
     $original_array = ["35","37","43","30","24","47","27"];
@@ -12,4 +14,30 @@
     echo "<pre>";
     print_r($new_array);
     
+?>
+
+<!-- Method-2: using for loop -->
+
+<?php
+
+    $myArray = ["35", "37", "43", "30", "24", "47", "27"];
+
+    $newArray = array();
+    $subArray = array();
+
+    for ($i = 0; $i < count($myArray); $i++) {
+        array_push($subArray, $myArray[$i]);
+        if (count($subArray) == 3) {
+            array_push($newArray, $subArray);
+            $subArray = array();
+        }
+    }
+
+    // Add remaining elements if any here 27 will remain
+    if (count($subArray) > 0) {
+        array_push($newArray, $subArray);
+    }
+    echo "<pre>";
+    print_r($newArray);
+
 ?>
