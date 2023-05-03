eb3c2a746872d2b1a9b48d2804f9728624f35798
Ronit Zinzuvadiya <96730598+RonitZinzuvadiya1040@users.noreply.github.com>
2023-05-03 10:57:31 +0530

    Update Task3.php

--- Rankway/Array Task/Task3.php
+++ Rankway/Array Task/Task3.php
@@ -3,6 +3,8 @@
     A: ["PETER"=>"35","BEN"=>"37","JOE"=>"43"]; 
 -->
 
+
+<!-- Method-1: using function -->
 <?php
     
     $array = array("Peter" => "35", "Ben" => "37", "Joe" => "43");
@@ -22,7 +24,32 @@
         // $upper_key = strtoupper($key);
         // $upper_keys[] = $upper_key;
     }
+    echo "Using Function";
     echo "<pre>";
     print_r($newArray);
+
+?>
+
+<!-- Methos-2 without function -->
+<?php
+
+    $array = ["Peter"=>"35", "Ben"=>"37", "Joe"=>"43"];
+
+    $new_array = array();
+
+    foreach ($array as $key => $value) {
+        $new_key = "";
+        for ($i = 0; $i < strlen($key); $i++) {
+            $char = $key[$i];
+            if ($char >= 'a' && $char <= 'z') {
+                $char = chr(ord($char) - 32); // convert lowercase letter to uppercase ASCII code
+            }
+            $new_key .= $char;
+        }
+        $new_array[$new_key] = $value; // add key-value pair to new array
+	}
+	echo "Without Function";
+	echo "<pre>";
+	print_r($new_array);
 
 ?>
